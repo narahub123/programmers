ff57f9fb0b49b7dc0a629db97e30509ad61692ee
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-10-16 09:26:08 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,16 +1,16 @@
 function solution(n) {
     // 약수 구하기 
-    const remainer = [n];
+    const remainers = [];
     
     let x = 1;
     
-    while(x < n){
+    while(x <= n){
         if(n % x === 0){
-            remainer.push(x);
+            remainers.push(x);
         }
         
         x++;
     }
     
-    return remainer.reduce((acc, cur) => acc + cur);
+    return remainers.reduce((acc, cur) => acc + cur, 0);
 }
