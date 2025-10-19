ea2f635899d8fad473cceb23f02493ef2bc3b67b
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-10-19 10:31:19 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,22 +1,23 @@
 function solution(a, b) {
     
-    if(a === b){
-        return a
-    } else {
-        // a, b 사이의 정수 배열 
-        // 절대값 구하기 (두 수의 차이)
-        const length = Math.abs(a - b) + 1;
+//     if(a === b){
+//         return a
+//     } else {
+//         // a, b 사이의 정수 배열 
+//         // 절대값 구하기 (두 수의 차이)
+//         const length = Math.abs(a - b) + 1;
         
-        // 작은 수 
-        const min = Math.min(a, b);
+//         // 작은 수 
+//         const min = Math.min(a, b);
         
-        // 두 수 사이의 정수 배열
-        const nums = Array.from({length}).map((_, idx) => min + idx);
+//         let sum = 0;
+        
+//         // 두 수 사이의 정수 배열
+//         const nums = Array.from({length}).forEach((_, idx) => sum += (min + idx));
         
         
-        // 두 수 사이의 정수 합
-        const sum = nums.reduce((acc, cur) => acc + cur, 0);
-        
-        return sum;
-    }
+//         return sum;
+//     }
+    
+    return (a + b) * (Math.abs(a - b) + 1) / 2
 }
