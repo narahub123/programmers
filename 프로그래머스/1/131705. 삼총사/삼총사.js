4c420b2a515d3eeccea2d05ff94ff3642cff12d6
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-10-21 12:41:48 +0900

    [level 1] Title: 삼총사, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+    let count = 0;
+    
+    for(let i = 0; i < numbers.length -2; i++){
+        for(let j = i + 1; j < numbers.length -1; j++){
+            for(let k = j + 1; k < numbers.length; k++){
+                const sum = numbers[i] + numbers[j] + numbers[k];
+                
+                if(sum === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
