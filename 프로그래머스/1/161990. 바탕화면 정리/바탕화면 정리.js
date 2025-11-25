544675c4052f0ecc426b7edaa6d9b71716a830a8
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-11-25 09:57:15 +0900

    [level 1] Title: 바탕화면 정리, Time: 33.70 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,38 +1,24 @@
 function solution(wallpaper) {
+    console.log("바탕화면 상태", wallpaper);
+    let minX = Infinity
+    let maxX = -Infinity;
+    let minY = Infinity;
+    let maxY = -Infinity;
     
-    const xs = [];
-    const ys = [];
-    
-    // 어떻게 좌표를 추출해야 하는가?
-    for(let y = 0; y < wallpaper.length; y++){
-        const row = wallpaper[y];
-        
-        for(let x = 0; x < row.length; x++){
-            // 좌표 
-            const coord = row[x];
-            
-            // 좌표가 빈칸인 경우 넘어가기 
-            if(coord === ".") continue;
-        
-            // 좌표에 파일 있는 경우 
-            ys.push(y);
-            xs.push(x);
+    for(let i = 0; i < wallpaper.length; i++){
+        for(let j = 0; j < wallpaper[0].length; j++){
+            if(wallpaper[i][j] === "#"){
+                console.log("y좌표", i);
+                console.log("x좌표", j);
+                
+                if(j < minX) minX = j;
+                if(maxX < j) maxX = j;
+                
+                if(i < minY) minY = i;
+                if(maxY < i) maxY = i;
+            }
         }
     }
     
-    // 좌표 모음
-    console.log(ys, xs);
-    
-    // 각 좌표의 최소, 최대값 
-    const minY = Math.min(...ys);
-    const maxY = Math.max(...ys);
-    const minX = Math.min(...xs);
-    const maxX = Math.max(...xs);
-    
-    console.log(minY, minX);
-    console.log(maxY + 1, maxX + 1);
-    
-    var answer = [minY, minX, maxY + 1, maxX + 1];
-    
-    return answer;
+    return [minY, minX, maxY + 1, maxX + 1];
 }
