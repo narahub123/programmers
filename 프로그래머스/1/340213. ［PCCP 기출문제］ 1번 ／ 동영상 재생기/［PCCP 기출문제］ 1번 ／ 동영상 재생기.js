2167886b4057c3c083fe9203f225d41dc40544d9
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-12-01 11:11:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,123 +1,71 @@
 function solution(video_len, pos, op_start, op_end, commands) {
-    console.log("현재 시간", pos);
+    const [vm, vs] = video_len.split(":").map(Number);
+    video_len = vm * 100 + vs;
+    let [pm, ps] = pos.split(":").map(Number);
+    pos = pm * 100 + ps;
+    const [sm, ss] = op_start.split(":").map(Number);
+    op_start = sm * 100 + ss;
+    const [em, es] = op_end.split(":").map(Number);
+    op_end = em * 100 + es;
     
-    
-    let [min, sec] = convertTime(pos);
-    
-    // 현재 시간이 오프닝 시간에 포함되는지 여부 확인
-    if(checkIsWithinOpening([min, sec], op_start, op_end)){
-        // 오프닝 시간에 포함되는 경우 오프닝의 완료 시간으로 이동 
-        const [endMin, endSec] = convertTime(op_end);
+    // 초기 위치가 오프닝 이전인 경우 
+    if(op_start <= pos && pos <= op_end){
+        pm = em;
+        ps = es;
         
-        min = endMin;
-        sec = endSec;
+        pos = pm * 100 + ps;
     }
-    
-    console.log('시간', min, sec);
     
     for(const command of commands){
         if(command === "prev"){
-            const [newMin, newSec] = moveTo10SecsBefore([min, sec]);
+            const subTen = ps - 10;
             
-            console.log("오프닝?", checkIsWithinOpening( [newMin, newSec], op_start, op_end))
-            
-            if(checkIsWithinOpening( [newMin, newSec], op_start, op_end)){
-                // 오프닝 시간에 포함되는 경우 오프닝의 완료 시간으로 이동 
-                const [endMin, endSec] = convertTime(op_end);
-                
-
-                min = endMin;
-                sec = endSec;
+            if(subTen < 0){
+                pm -= 1;
+                ps = 60 + subTen;
             } else {
-                 min = newMin;
-                 sec = newSec;
+                ps = subTen;
             }
             
-           
-        } else {
-            const [newMin, newSec] = moveTo10SecsAfter([min, sec], video_len);
-            
-            if(checkIsWithinOpening( [newMin, newSec], op_start, op_end)){
-                // 오프닝 시간에 포함되는 경우 오프닝의 완료 시간으로 이동 
-                const [endMin, endSec] = convertTime(op_end);
-
-                min = endMin;
-                sec = endSec;
-            } else {
-                 min = newMin;
-                 sec = newSec;
+            if(pm < 0){
+                pm = 0;
+                ps = 0;
             }
+            
+            pos = pm * 100 + ps;
+        }
+        
+        if(command === "next"){
+            const addTen = ps + 10;
+            
+            if(addTen >= 60){
+                pm +=1;
+                ps = addTen % 60;
+            } else {
+                ps = addTen;
+            }
+            
+            if(pm >= vm && ps >=vs){
+                pm = vm;
+                ps = vs;
+            }
+            
+            pos = pm * 100 + ps;
+        }
+        
+        if(op_start <= pos && pos <= op_end){
+            pm = em;
+            ps = es;
+            pos = pm * 100 + ps;
         }
     }
-    
-     console.log('시간', min, sec);
-    var answer = `${min.toString().padStart(2, 0)}:${sec.toString().padStart(2, 0)}`;
-    return answer;
-}
-
-// 시간을 숫자 배열로 변환
-const convertTime = (time) => {
-    return time.split(':').map(Number);
-}
-
-// 오프닝 안의 시간 여부 확인 
-const checkIsWithinOpening = (curTime, op_start, op_end) => {
-    const [min, sec] = curTime;
-    const [startMin, startSec] = convertTime(op_start);
-    const [endMin, endSec] = convertTime(op_end);
-    
-    if(startMin <= min && min <= endMin){
-        if(startMin === endMin){
-            if(startSec <= sec && sec <= endSec){
-                return true;    
-            }
-            return false;
-        } else if(startMin !== endMin){
-            if(startMin === min){
-                if(startSec <= sec){
-                    return true;
-                }
-                return false
-            } else if(min === endMin) {
-                if(sec <= endSec){
-                    return true;
-                }
-                return false;
-            } 
-            return true;
-        } 
+   
+    if(op_start <= pos && pos <= op_end){
+        pm = em;
+        ps = es;
+        pos = pm * 100 + ps;
     }
     
-    return false;
+    return `${pm.toString().padStart(2, '0')}:${ps.toString().padStart(2, '0')}`
 }
 
-// 10초 미만 여부 확인
-const moveTo10SecsBefore = (curTime) => {
-    let [min, sec] = curTime;
-    
-    if(min === 0 && sec <10){
-        return [0, 0];
-    }
-    
-    if(sec - 10 < 0){
-        min--;
-        sec = 50 + sec;
-    } else {
-        sec -= 10;
-    }
-    
-    return [min, sec];
-}
-
-// 완료 10초 미만 여부 확인
-const moveTo10SecsAfter = (curTime, length) => {
-  const [min, sec] = curTime;
-  const [lenMin, lenSec] = convertTime(length);
-
-  const curTotal = min * 60 + sec;
-  const lenTotal = lenMin * 60 + lenSec;
-
-  const next = Math.min(curTotal + 10, lenTotal);
-
-  return [Math.floor(next / 60), next % 60];
-};
