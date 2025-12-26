dedc2248104474a3b5ce0533e3ae404f250c48bc
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-12-26 10:38:14 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 45.03 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    const answer = Array(numbers.length).fill(-1);
+    
+    const stack = [];
+    
+    for(let i = 0; i < numbers.length; i++){
+    
+        while(stack.length && numbers[stack[stack.length - 1]] < numbers[i]){
+            const idx = stack.pop();
+            answer[idx] = numbers[i];
+        }
+        stack.push(i);
+    }
+    
+    return answer;
+}
