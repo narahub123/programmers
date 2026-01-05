c34d1226df20c3dec58d9e84653dbd3b68ea2aef
Naraaaa <93567002+narahub123@users.noreply.github.com>
2026-01-05 10:05:11 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.58 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,17 @@
+const MODULER = 1000000007;
+
+function solution(n) {
+    const dp = Array(n + 1).fill(0);
+    
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(let i = 2; i <= n; i++){
+        
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MODULER;
+    }
+    
+    
+    
+    return dp[n]
+}
